71537519de9f32ec815d913aef47c3e37f2c9155
Oscar Gonzalez <oscarrrr12@gmail.com>
2024-09-13 22:09:43 +0200

    Changed app.config, added interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { httpInterceptorProviders } from './_helpers/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())]
+  providers: [provideRouter(routes), provideHttpClient(withFetch()),httpInterceptorProviders]
 };
