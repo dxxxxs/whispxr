db3f6778298f67513f905919b050608bbf9563a5
Oscar Gonzalez <oscarrrr12@gmail.com>
2024-09-13 22:11:54 +0200

    Updated providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { httpInterceptorProviders } from './_helpers/http.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()),httpInterceptorProviders]
+  providers: [provideRouter(routes), provideHttpClient(withFetch()),httpInterceptorProviders, provideAnimationsAsync(), {provide: LocationStrategy, useClass: HashLocationStrategy}]
 };
